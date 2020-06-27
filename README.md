e47cbdae9e668f43708af51c40e263ee0cd3dd2e
lukelin1991 <lukelin1991@yahoo.com>
2020-06-27 16:22:27 -0400

    setting up routes

--- README.md
+++ README.md
@@ -2,23 +2,21 @@
 
 
 Application Goals:
-I want to learn Node.JS + Express + MongoDB.
+I want to learn Node.JS + Express + MongoDB (with mongoose).
 to leverage with my frontend React.js skills, to create a full stack application.
 
 This will be:
 A full stack application to store and list places you have traveled.
 
-Data:
-- Log Entry
-
+Data - Log Entry:
 - Title - Text
 - Description - Text
 - Comments - Text
 - Rating - scale? 1-10
 - Image - Text - URL
 
-- Latitude? - number?
-- Longitude? - number?
+- Latitude? - number
+- Longitude? - number
 
 - Created At - DateTime
 - Updated At - DateTime
--- server/src/index.js
+++ server/src/index.js
@@ -2,21 +2,35 @@ const express = require('express');
 const morgan = require('morgan')
 const helmet = require('helmet'); 
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const middlewares = require('./middlewares');
+const logs = require('./api/logs')
+
+require('dotenv').config();
 
 const app = express();
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
 }));
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({
         message: `Hello World!`,
     });
 });
+
+app.use('/api/logs', logs)
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
