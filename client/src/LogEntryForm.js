3437d6a9cad283d7a404dd45db867c736c688059
lukelin1991 <lukelin1991@yahoo.com>
2020-06-30 17:28:13 -0400

    api key

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -27,18 +27,6 @@ const LogEntryForm = ({ location, onClose }) => {
             {error ? <h3 className="error">{error}</h3> : null}
             <label htmlFor="apiKey">API KEY</label>
             <input type="password" name="apiKey" required ref={register}/>
-            <label htmlFor="title">Title</label>
-            <input name="title" required ref={register} />
-
-            <label htmlFor="comments">Comments</label>
-            <textarea name="comments" rows={3} ref={register}></textarea>
-            <label htmlFor="description">Description</label>
-            <textarea name="description" rows={3} ref={register}></textarea>
-            <label htmlFor="image">Image</label>
-            <input name="image" ref={register} />
-            <label htmlFor="visitDate">Visit Date</label>
-            <input name="visitDate" type="date" required ref={register} />
-            <button disabled={loading}> {loading ? 'Loading...' : 'Create Entry'} </button>
         </form>
     );
 }
