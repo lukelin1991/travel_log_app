05b84947ad01dc8ed230db58ab78c48d676cc86b
lukelin1991 <lukelin1991@yahoo.com>
2020-06-30 17:27:32 -0400

    created form and use mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
-
+import LogEntryForm from './LogEntryForm';
 import { listLogEntries } from './API'
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -14,12 +15,22 @@ const App = () => {
     zoom: 3
   });
 
+  const getEntries = async () => {
+    const logEntries = await listLogEntries();
+    setLogEntries(logEntries)
+  }
+
   useEffect(() => {
-    (async () => {
-      const logEntries = await listLogEntries();
-      setLogEntries(logEntries);
-    })();
+    getEntries();
   }, []);
+
+  const showAddMarkerPopup = (event) => {
+    const [ longitude, latitude ] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
+  };
 
   return (
     <ReactMapGL
@@ -27,10 +38,11 @@ const App = () => {
       mapStyle="mapbox://styles/lukelin1991/ckc14ljw8140q1iqojbvfjdss"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewPortChange={setViewport}
+      onDblClick={showAddMarkerPopup}
     >
       {
         logEntries.map(entry => (
-          <React.Fragment key={entry._id} >
+          <React.Fragment key={entry._id}>
             <Marker 
               latitude={entry.latitude} 
               longitude={entry.longitude} 
@@ -39,17 +51,23 @@ const App = () => {
                 onClick={() => setShowPopup({
                   [entry._id]: true,
                 })}
-              
               >
-                <img
-                  className="marker"
+                <svg
+                  className="marker yellow"
                   style={{
                     height: `${6 * viewport.zoom}px`,
                     width: `${6 * viewport.zoom}px`,
                   }}
-                  src="https://i.imgur.com/y0G5YTX.png"
-                  alt="marker"
-                />
+                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                  <g>
+                    <g>
+                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                    </g>
+                  </g>
+                </svg>
+              </div>
             </Marker>
             {
               showPopup[entry._id] ? (
@@ -74,9 +92,49 @@ const App = () => {
           </React.Fragment>
         ))
       }
+      {
+        addEntryLocation ? (
+          <>
+          <Marker
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+          >
+            <div>
+              <svg
+                className="marker red"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                <g>
+                  <g>
+                    <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setAddEntryLocation(null)}
+            anchor="top"
+          >
+            <div className="popup">
+                
+            </div>
+          </Popup>
+          </>
+        ) : null
+      }
     </ReactMapGL>
-  
-  )
+  );
 };
 
 export default App;
